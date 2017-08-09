335fe59fc5e0def2dc83c6dc29661dec02decb3b
acer-pc <chijieming@sina.com>
2017-08-09 19:28:09 +0800

    main.js

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -14,8 +14,14 @@ describe("测试描述", function(){
 
     it("print out “number bottles of beer on the wall” song when the number is more than 1", function(){
 
-        var result = main(2);
-        var expect_string = `2 bottles of beer on the wall, 2 bottles of beer.
+        var result = main(5);
+        var expect_string = `5 bottles of beer on the wall, 5 bottles of beer.
+Take one down and pass it around, 4 bottles of beer on the wall.
+4 bottles of beer on the wall, 4 bottles of beer.
+Take one down and pass it around, 3 bottles of beer on the wall.
+3 bottles of beer on the wall, 3 bottles of beer.
+Take one down and pass it around, 2 bottles of beer on the wall.
+2 bottles of beer on the wall, 2 bottles of beer.
 Take one down and pass it around, 1 bottle of beer on the wall.
 1 bottle of beer on the wall, 1 bottle of beer.
 Take one down and pass it around, no more bottles of beer on the wall.
